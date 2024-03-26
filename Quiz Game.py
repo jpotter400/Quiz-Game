695e15ad1c38e85897a850c741e712b94aa51033
jpotter400 <143131448+jpotter400@users.noreply.github.com>
2024-03-26 17:11:35 -0500

    Add files via upload

---
+++ Quiz Game.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Welcome to my computer quiz!")
+
+
+# In[2]:
+
+
+playing = input("Would you like to play my game? ")
+
+
+# In[19]:
+
+
+print("Sweet! Let's play!")
+score = 0
+
+
+# In[20]:
+
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect!")
+
+
+# In[21]:
+
+
+answer = input("What does GPU stand for? ")
+if answer == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect!")
+
+
+# In[22]:
+
+
+answer = input("What does RAM stand for? ")
+if answer == "random access memory":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect!")
+
+
+# In[23]:
+
+
+answer = input("What does PSU stand for? ")
+if answer == "power supply":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect!")
+
+
+# In[24]:
+
+
+print("You got " + str(score) + " Questions Correct!")
+
+
+# In[29]:
+
+
+print("You scored a " + str((score / 4) * 100) + "%.")
+if score >= 50.0:
+    print("Great Job!")
+else:
+    print("Back to the drawing board!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
